7385683b5df218911d8fb801b4e2f89fd9136da9
Donny <EDL1155@gmail.com>
2020-01-26 09:07:52 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+function writeCards(arrayOfNames, eventName) {
+   const messageArray = [];
+
+   for (let i = 0; i < arrayOfNames.length; i++) {
+     let individualName = arrayOfNames[i];
+
+     let individualMessage = `Thank you, ${individualName}, for the wonderful ${eventName} gift!`;
+     messageArray.push(individualMessage);
+   }
+
+   return messageArray;
+
+}
+
+const countDown = (num) => {
+
+let i = num;
+  while (i >= 0) {
+    console.log(i)
+    i--
+  }
+}
